c99c6ead589f85f33e13106652e82dc55ea01ebe
saksham1895garg <gargsaksham1895@gmail.com>
2025-07-20 23:13:32 +0530

    Initial commit4

--- components/Section2.tsx
+++ components/Section2.tsx
@@ -16,23 +16,23 @@ const Section2 = () => {
                 </div>
                 <div className="insta-conn">
                     <div className="insta1">
-                        <InstagramEmbed2 permalink='https://www.instagram.com/reel/DIG5S7FNn8V/?utm_source=ig_embed&amp;utm_campaign=loading' />
+                        <InstagramEmbed2 permalink="https://www.instagram.com/reel/DIG5S7FNn8V/?utm_source=ig_embed&amp;utm_campaign=loading" />
                     </div>
                     <div className="insta1">
-                        <InstagramEmbed2 permalink='https://www.instagram.com/reel/DHycYOUBAx7/?utm_source=ig_embed&amp;utm_campaign=loading' />
+                        <InstagramEmbed2 permalink="https://www.instagram.com/reel/DHycYOUBAx7/?utm_source=ig_embed&amp;utm_campaign=loading" />
                     </div>
                     <div className="insta1">
-                        <InstagramEmbed2 permalink='https://www.instagram.com/reel/DEPGdxGBg6m/?utm_source=ig_embed&amp;utm_campaign=loading' />
+                        <InstagramEmbed2 permalink="https://www.instagram.com/reel/DEPGdxGBg6m/?utm_source=ig_embed&amp;utm_campaign=loading" />
                     </div>
                     <div className="insta1">
-                        <InstagramEmbed2 permalink='https://www.instagram.com/reel/C_8GtC_pqQu/?utm_source=ig_embed&amp;utm_campaign=loading' />
+                        <InstagramEmbed2 permalink="https://www.instagram.com/reel/C_8GtC_pqQu/?utm_source=ig_embed&amp;utm_campaign=loading" />
                     </div>
                 </div >
             </section >
             <section className='apply-now-section'>
                 <h2 className='ready-to-join'>Ready to Join HyperVision?</h2>
                 <p className='apply-now-info'>Become part of a community that's shaping the future through innovation and collaboration.</p>
-                <button className='apply-now-btn' onClick={() => window.open('https://forms.gle/hypervisionApplicationForm', '_blank')}>Apply Now</button>
+                <button className='apply-now-btn' onClick={() => window.open("https://forms.gle/hypervisionApplicationForm", "_blank")}>Apply Now</button>
             </section>
         </>
     )
